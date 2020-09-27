5ef91e6e5bfc739c5f4803957bb1c85696bcfca5
Shoya HARA <contact@halrs.com>
2020-09-27 21:25:19 +0900

    D言語テンプレート入力関数の簡単化

--- template/main.d
+++ template/main.d
@@ -2,19 +2,11 @@ void solve(){
 
 }
 void main(){
-	writeln()
+	inln!long().writeln();
 }
      
 import std.stdio, std.conv, std.algorithm, std.numeric, std.string, std.math, std.range;
 const long mod = 10^^9+7;      
-// 1要素のみの入力
-T inelm(T= int)(){
-	return to!(T)( readln().chomp() );
-}
-     
-// 1行に同一型の複数入力
-T[] inln(T = int)(){
-	T[] ln;
-	foreach(string elm; readln().chomp().split())ln ~= elm.to!T();
-	return ln;
-}
+T inelm(T= int)(){ return to!(T)( readln().chomp() ); }
+T[] inary(T = int)(){ return readln().chomp().split().to!(T[])(); }
+
