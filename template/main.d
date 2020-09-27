a024469c8d9c89086e300cb5e81780aa3570898f
Shoya HARA <contact@halrs.com>
2020-09-27 21:27:42 +0900

    文字列入力エイリアスの追加

--- template/main.d
+++ template/main.d
@@ -1,12 +1,11 @@
 void solve(){
-
 }
 void main(){
-	inln!long().writeln();
 }
      
 import std.stdio, std.conv, std.algorithm, std.numeric, std.string, std.math, std.range;
-const long mod = 10^^9+7;      
+const long mod = 10^^9+7;
+alias instr = () => readln().chomp();
 T inelm(T= int)(){ return to!(T)( readln().chomp() ); }
 T[] inary(T = int)(){ return readln().chomp().split().to!(T[])(); }
 
