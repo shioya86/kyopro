525af8083ece9727e878fe33a0d85e93cbb2b951
Shoya HARA <contact@halrs.com>
2020-10-06 02:23:15 +0900

    D言語テンプレートの変更

--- template/main.d
+++ template/main.d
@@ -1,11 +1,18 @@
 void solve(){
 }
+
 void main(){
 }
      
-import std.stdio, std.conv, std.algorithm, std.numeric, std.string, std.math, std.range, std.typecons;
+import std.stdio, std.conv, std.algorithm, std.numeric, 
+			 std.string, std.math, std.range, std.typecons;
 const long mod = 10^^9+7;
-alias instr = () => readln().chomp();
-T inelm(T= int)(){ return to!(T)( readln().chomp() ); }
-T[] inary(T = int)(){ return readln().chomp().split().to!(T[])(); }
+const long inf = 10L^^18+1;
 
+alias Tuple!(int, "l", int, "r") Pair;
+Pair mkPair(int[] lr){ Pair p; p.l=lr[0]; p.r=lr[1]; return p; }
+
+alias instr = () => readln.chomp;
+T inelm(T= int)(){ return readln.chomp.to!int; }
+T[] inarr(T = int)(){ return readln.split.to!(T[]); }
+int c2i(char c){ return c.to!string.to!int; }
