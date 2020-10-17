8d2c52653b39a02710e3df60971a60e7b0a232a2
Shoya HARA <contact@halrs.com>
2020-10-17 19:36:06 +0900

    inelm関数の型変換のミスの修正

--- template/main.d
+++ template/main.d
@@ -13,6 +13,6 @@ alias Tuple!(int, "l", int, "r") Pair;
 Pair mkPair(int[] lr){ Pair p; p.l=lr[0]; p.r=lr[1]; return p; }
 
 alias instr = () => readln.chomp;
-T inelm(T= int)(){ return readln.chomp.to!int; }
+T inelm(T= int)(){ return readln.chomp.to!T; }
 T[] inarr(T = int)(){ return readln.split.to!(T[]); }
 int c2i(char c){ return c.to!string.to!int; }
