52244789dff101d8499076e7f9c1ffedd3385046
Shoya HARA <halrs816@gmail.com>
2020-10-26 03:09:19 +0900

    update template

--- template/main.d
+++ template/main.d
@@ -1,6 +1,3 @@
-oyarare/coop-sim-drawervoid solve(){
-}
-
 void main(){
 }
      
@@ -12,7 +9,8 @@ alias Tuple!(int, "l", int, "r") Pair;
 Pair mkPair(int[] lr){ Pair p; p.l=lr[0]; p.r=lr[1]; return p; }
 
 alias instr = () => readln.chomp;
-T inelm(L...)(ref L A){ auto l = readln.split; 
+T inone(T=int)(){return readln.chomp.to!T;}
+void inelm(L...)(ref L A){ auto l = readln.split;
 	foreach(i, T; L) A[i]=l[i].to!T; }
 T[] inarr(T = int)(){ return readln.split.to!(T[]); }
 int c2i(char c){ return c.to!string.to!int; }
