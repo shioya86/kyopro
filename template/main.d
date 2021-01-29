43f60248d303b9aec5a410340dc67dcdaadbbf98
Shoya HARA <shioyarare@gmail.com>
2021-01-30 02:53:07 +0900

    fix: Fixed processing to convert char type to number

--- template/main.d
+++ template/main.d
@@ -12,23 +12,4 @@ T inone(T=int)(){return readln.chomp.to!T;}
 void inelm(L...)(ref L A){ auto l = readln.split;
 	foreach(i, T; L) A[i]=l[i].to!T; }
 T[] inarr(T = int)(){ return readln.split.to!(T[]); }
-int c2i(char c){ return c.to!string.to!int; }
-
-// Prime factorization
-long[long] primeFactorize(long n)
-{
-	long[long] res;
-	for(long i=2; i*i <= n; i++)
-	{
-		if(n%i != 0) continue;
-		long tmp;
-		while(n%i==0)
-		{
-			tmp++;
-			n /= i;
-		}
-		res[i] += tmp;
-	}
-	if( n!=1 ) res[n]++; 
-	return res;
-}
+int convert_num(T=int)(char c){ return (c-'0').to!T; }
