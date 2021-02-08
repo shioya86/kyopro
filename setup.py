7960cf203e5639c1c6da8f3126d5ce75a3d74d20
Shoya HARA <shioyarare@gmail.com>
2021-02-08 17:35:12 +0900

    refactor: Obtain the OS name automatically.

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ def create_desktop_path(os_name):
     desktop_path = ''
     if os_name in os_win:
         desktop_path = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Desktop"
-    elif dep in os_linux:
+    elif platform.system() in os_linux:
         desktop_path = ''
-    elif dep in os_mac:
+    elif platform.system() in os_mac:
         desktop_path = ''
     return desktop_path
 
@@ -36,12 +36,10 @@ def input_default(default):
     return default if res=='' else res
 
 if __name__ == '__main__':
+    platform.system()
     print( '!input your project information')
-    print( '(1) Windows\n(2) Mac\n(3) Ubuntu')
-    print( "Your OS?", end=' ' )
-    dep = input_default(os_name)
-    print( 'Directory?', end=' ')
-    path = input_default( create_desktop_path(dep) )
+    print( 'File path?', end=' ')
+    path = input_default( create_desktop_path(platform.system()) )
     print( '(1) D (default) \n(2) Python \n(3) C++')
     print( 'Programming language?', end=' ' )
     lang = input_default('d')
@@ -56,7 +54,7 @@ if __name__ == '__main__':
     print( 'making competitive programming project ...')
 
     # 解答テンプレートの挿入
-    if dep in os_win:
+    if platform.system() in os_win:
         # For Windows OS
         order = 'mkdir {}\\{}'.format(path, outdir)
         subprocess.run(order, shell=True) # make target directory
@@ -69,12 +67,12 @@ if __name__ == '__main__':
         order = 'copy {} {}'.format(template_path, out_path)
         create_files(_lang=lang, _order=order)
 
-    elif dep in os_linux:
+    elif platform.system() in os_linux:
         # For Linux OS bash
         print('[error]Unimplemented')
         exit()
 
-    elif dep in os_mac:
+    elif platform.system() in os_mac:
         # For Mac OS
         print('[error]Unimplemented')
         exit()
