7fa6e66ba89d6cc045192872d2a2a8f290d47050
Shoya HARA <shioyarare@gmail.com>
2021-02-23 23:59:01 +0900

    style: convert_num renamed

--- template/main.d
+++ template/main.d
@@ -11,6 +11,6 @@ T inone(T=int)(){return readln.chomp.to!T;}
 void inelm(L...)(ref L A){ auto l = readln.split;
 	foreach(i, T; L) A[i]=l[i].to!T; }
 T[] inarr(T = int)(){ return readln.split.to!(T[]); }
-T convert_num(T=int)(char c){ return (c-'0').to!T; }
+T convn(T=int)(char c){ return (c-'0').to!T; }
 bool chmin(T=long)(ref T a, const T b) { if(a>b) {a=b; return true;} return false; }
 bool chmax(T=long)(ref T a, const T b) { if(a<b) {a=b; return true;} return false; }
