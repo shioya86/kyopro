617c4150bf83a676dc62d7892c2ac502b336af04
Shoya HARA <shioyarare@gmail.com>
2021-02-23 23:58:13 +0900

    feat: add chmin, chmax

--- template/main.d
+++ template/main.d
@@ -12,3 +12,5 @@ void inelm(L...)(ref L A){ auto l = readln.split;
 	foreach(i, T; L) A[i]=l[i].to!T; }
 T[] inarr(T = int)(){ return readln.split.to!(T[]); }
 T convert_num(T=int)(char c){ return (c-'0').to!T; }
+bool chmin(T=long)(ref T a, const T b) { if(a>b) {a=b; return true;} return false; }
+bool chmax(T=long)(ref T a, const T b) { if(a<b) {a=b; return true;} return false; }
