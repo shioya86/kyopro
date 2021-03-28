075d3a04ae125136568d7f77f75566a3bc9b25af
Shoya HARA <shioyarare@gmail.com>
2021-03-28 17:33:33 +0900

    style: union find more shorter

--- template/main.d
+++ template/main.d
@@ -17,36 +17,14 @@ bool chmin(T=long)(ref T a, const T b) { if(a>b) {a=b; return true;} return fals
 bool chmax(T=long)(ref T a, const T b) { if(a<b) {a=b; return true;} return false; }
 
 class UnionFind {
-	int[] par;
-	int[] rank;
-
-	this(int n) {
-		par.length = n;
-		rank.length = n;
-		foreach(i; 0..n) par[i] = i;
-	}
-
-	int find(int x) {
-		if (par[x] == x) 
-			return x;
-		else 
-			return par[x] = find(par[x]);
-	}
-
+	int[] par, rank;
+	this(int n) { par.length = rank.length = n; foreach(i; 0..n) par[i] = i; }
+	int find(int x) { if (par[x] == x) return x; else return par[x] = find(par[x]); }
 	void unite(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if(x==y) 
-			return;
-		if(rank[x] < rank[y])
-			par[x] = y;
-		else {
-			par[y] = x;
-			if (rank[x] == rank[y]) rank[x]++;
-		}
+		x = find(x); y = find(y);
+		if(x==y) return;
+		if(rank[x] < rank[y]) par[x] = y;
+		else { par[y] = x; if (rank[x] == rank[y]) rank[x]++; }
 	}
-
-	bool same(int x, int y) {
-		return find(x) == find(y);
-	}
+	bool same(int x, int y) { return find(x) == find(y); }
 }
