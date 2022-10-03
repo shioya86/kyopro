40a9007554f18259134e5960772b0c619f373a43
Shoya HARA <shara@ml.harashio.jp>
2022-10-04 04:51:31 +0900

    add: input, output等

--- template/main.rb
+++ template/main.rb
@@ -1,3 +1,12 @@
+# input
+def instr(); gets.chomp end
+def inarr(sym=:to_i); gets.split.map(&sym) end
+def inone(sym=:to_i); gets.send(sym) end
+# output
+def printYn(flg); puts flg ? "Yes" : "No" end 
+# utils
+def max(*e); e.max end
+def min(*e); e.min end
 
 def init()
 
@@ -13,4 +22,3 @@ def main()
 end
 
 main
-
