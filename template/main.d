0fbd37e33b14c734e5c43a20dddf866a7a04f8fe
shara <shara@ml.harashio.jp>
2023-09-01 23:01:30 +0900

    fix: ModIntが式の中で自身の値を書き換えてしまうバグを修正

--- template/main.d
+++ template/main.d
@@ -36,16 +36,18 @@ class UnionFind {
 }
 alias mint = ModInt!(MOD);
 struct ModInt (long M) { 
-  long n;
-  this(T)(T n) {this.n = n%M;}
+  private long n;
+  this(T)(T n) { this.n = n%M; }
   void opUnaray (string OP)() { if (OP == "++") this.n = sum(this.n, 1); if (OP == "--") this.n = sub(this.n, 1); }
-  void opOpAssign (string OP)(ModInt r) { if (OP == "+")  this.n = sum(this.n, r.n); if (OP == "-")  this.n = sub(this.n, r.n); if (OP == "*")  this.n = mul(this.n, r.n); if (OP == "/")  this.n = div(this.n, r.n); if (OP == "^^") this.n = pow(this.n, r.n); }
   void opOpAssign (string OP)(long r) { this.opOpAssign!OP( ModInt(r) ); }
-  void opAssign( long r ){ this.n = r; }
-  auto opBinary( string OP )( ModInt r ) { this.opOpAssign!OP(r); return this; }
-  auto opBinary( string OP )( long r ){ return this.opBinary!OP( ModInt(r) ); }
+  void opAssign (long r) { this.n = r; }
+  auto opBinary (string OP: "+")(ModInt r) { return ModInt(sum(this.n, r.n)); }
+  auto opBinary (string OP: "-")(ModInt r) { return ModInt(sub(this.n, r.n)); }
+  auto opBinary (string OP: "*")(ModInt r) { return ModInt(mul(this.n, r.n)); }
+  auto opBinary (string OP: "/")(ModInt r) { return ModInt(div(this.n, r.n)); }
+  auto opBinary (string OP)(long r){ return this.opBinary!OP( ModInt(r) ); }
+  long get () { return this.n; }
   auto opBinaryRight( string OP )( long l ){ return ModInt(l).opBinary!OP(this); }
-  string toString() { return this.n.to!string; }
   long sum (long l, long r) { return (l + r) % M; } long sub (long l, long r) { return (l + M - r) % M; }
   long div (long l, long r) { return (l * pow(r, M - 2)) % M; } long mul (long l, long r) { return (l * r) % M; }
   long pow (long l, long r) { if (r == 1) return l; if (r & 1) return (l * pow(l, r - 1)) % M; return pow(l, r/2)^^2 % M; }
