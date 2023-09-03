8ed5e8b339a18991ae6b94c87d7498dbfe463887
shara <shara@ml.harashio.jp>
2023-09-03 18:30:14 +0900

    feat UnionFind にサイズを取得する関数を追加

--- template/main.d
+++ template/main.d
@@ -28,11 +28,12 @@ bool chmax (T=long)(ref T a, const T b) { if(a<b) {a=b; return true;} return fal
 // data structure
 alias PQueue(T, alias less="a<b") = BinaryHeap!(Array!T, less);
 class UnionFind {
-  int[] par, rank;
-  this(int n) { par.length = rank.length = n; foreach(i; 0..n) par[i] = i; }
-  int find(int x) { if (par[x] == x) return x; else return par[x] = find(par[x]); }
-  void unite(int x, int y) { x = find(x); y = find(y); if (x==y) return; if (rank[x] < rank[y]) par[x] = y; else { par[y] = x; if (rank[x] == rank[y]) rank[x]++; } }
+  int[] par, rank, cnt;
+  this(int n) { par.length=rank.length=cnt.length=n;cnt[]=1;foreach(i;0..n)par[i]=i; }
+  int find(int x) { if(par[x] == x)return x;else return par[x]=find(par[x]); }
+  void unite(int x, int y) { x=find(x);y=find(y);if(x==y)return;if(rank[x]<rank[y]){par[x]=y;cnt[y]+=cnt[x];}else{par[y]=x;cnt[x]+=cnt[y];if(rank[x]==rank[y])rank[x]++;} }
   bool same(int x, int y) { return find(x) == find(y); }
+  int size(int x) { return cnt[find(x)]; }
 }
 alias mint = ModInt!(MOD);
 struct ModInt (long M) { 
